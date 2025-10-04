7997050ed465eec6b5ad19c8c4482d41d67aa69c
Kamil <kdnuriev@gmail.com>
2025-10-04 19:44:31 +0400

    basic frontend

---
+++ static/main.js
@@ -0,0 +1,25 @@
+const lat = 41.711033;
+const lon = 44.758182;
+const zoom = 13;
+const map = L.map('map').setView([lat, lon], zoom);
+
+//Basic set up for Leaflet map
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+//Add default marker
+var marker = L.marker([lat, lon]).addTo(map);
+
+//Add marker on map click
+function onMapClick(e) {
+    map.removeLayer(marker);
+    marker = L.marker(e.latlng).addTo(map); 
+    document.getElementById("lat").value = e.latlng.lat;
+    document.getElementById("lon").value = e.latlng.lng;
+}
+
+function handler(event){alert("AAAAAAAAAAAAAAAAAAAa")}
+
+map.on('click', onMapClick);
